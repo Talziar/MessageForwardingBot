13348430d10f612ec7da6563abb8abb25daacf53
Talziar <71977032+Talziar@users.noreply.github.com>
2024-03-17 00:18:16 +0300

    Update main.py

--- message_forwarding_bot/modules/main.py
+++ message_forwarding_bot/modules/main.py
@@ -1,13 +1,11 @@
 """Bot main module."""
 
-from telethon import events
-
 from message_forwarding_bot.bot import BOT
 from message_forwarding_bot import USER_ID
 from message_forwarding_bot.utils.telegram import tg_exceptions_handler
 
 
-@BOT.on(events.NewMessage)
+@BOT.on_message()
 @tg_exceptions_handler
-async def start(event: events.NewMessage.Event) -> None:
-    await BOT.forward_messages(USER_ID, event.message)
+async def start(client, message) -> None:
+    await BOT.forward_messages(USER_ID, message.from_user.id, [message.id])
