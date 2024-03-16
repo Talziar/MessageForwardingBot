982d7c53f243510ff4610b10eb3282aa0cce694b
Talziar <71977032+Talziar@users.noreply.github.com>
2024-03-16 23:57:11 +0300

    Update bot.py

--- message_forwarding_bot/bot.py
+++ message_forwarding_bot/bot.py
@@ -1,8 +1,9 @@
 """Telegram Bot."""
 
+import asyncio
 import logging
 
-from pyrogram import Client
+from pyrogram import Client, idle
 
 from message_forwarding_bot import API_HASH, API_KEY, USERNAME
 from message_forwarding_bot.modules import ALL_MODULES
@@ -11,9 +12,26 @@ from message_forwarding_bot.utils.loader import load_modules
 LOGGER = logging.getLogger(__name__)
 BOT = Client(USERNAME, API_KEY, API_HASH)
 BOT_INFO = {}
-BOT.parse_mode = "markdown"
 
-LOGGER.info("Bot started!")
 
-load_modules(ALL_MODULES, __package__)
-BOT.run()
+def main() -> None:
+    BOT.run(run())
+
+
+async def run() -> None:
+    async with BOT:
+        bot_info = await BOT.get_me()
+        BOT_INFO.update(
+            {"name": bot_info.first_name, "username": bot_info.username, "id": bot_info.id}
+        )
+        LOGGER.info(
+            "Bot started as %s! Username is %s and ID is %s",
+            BOT_INFO["name"],
+            BOT_INFO["username"],
+            BOT_INFO["id"],
+        )
+        load_modules(ALL_MODULES, __package__)
+        await idle()
+    await BOT.stop()
+
+
